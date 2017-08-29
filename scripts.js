4726e9e9432d49c6f4dc376aa2a38e8f8fa2dd9c
robertwdowczyk <30527317+robertwdowczyk@users.noreply.github.com>
2017-08-29 13:49:46 +0200

    Karuzela

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
-﻿// scripts.js
-$("span:even").css('color', 'red');
-var paragraphs = $('p');
-paragraphs.each(function(index, element) {
-
-    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-    $(element).append(button)
-
-});
-$("button").click(function(){
-    alert($(this).attr("data-tmp"));
+﻿
+$(function(){
+    var carouselList = $("#carousel ul");
+  setInterval(changeSlide, 3000); 
+  function changeSlide() {
+    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+  };
+  changeSlide();
+  function moveFirstSlide() {
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem);
+    carouselList.css({marginLeft:0});
+  };
+  
 });
